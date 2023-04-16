f66a20c9e1d39952f87fd90b66c2321020e3fa62
Seefelder <2376637+seefelder@users.noreply.github.com>
2023-04-16 10:22:07 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatSwiss",
+  description: "Your personal ChatSwiss Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatSwiss Doctor",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
